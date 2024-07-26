fcaf8f2092e4ef0b70eab62b6a7fe727ceeacd2f
Dan <dansantosalfa@gmail.com>
2024-07-26 18:49:16 -0300

    feat: add rotas

--- controle-mercado-api/start/routes.js
+++ controle-mercado-api/start/routes.js
@@ -16,4 +16,52 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.post('signup', 'UserController.signup')
+Route.post('login', 'UserController.login')
+
+Route.group(() => {
+  // Client
+  Route.get('clients', 'ClientController.indexClients')
+  Route.get('clients/:id', 'ClientController.showClient')
+  Route.post('clients', 'ClientController.storeClient')
+  Route.put('clients/:id', 'ClientController.updateClient')
+  Route.delete('clients/:id', 'ClientController.deleteClient')
+
+  // Address
+  Route.get('addresses', 'ClientController.indexAddresses')
+  Route.get('addresses/:id', 'ClientController.showAddress')
+  Route.post('addresses', 'ClientController.storeAddress')
+  Route.put('addresses/:id', 'ClientController.updateAddress')
+  Route.delete('addresses/:id', 'ClientController.deleteAddress')
+  Route.get('clients/:client_id/addresses', 'ClientController.showAddressesByClientId')
+  Route.put('clients/:client_id/addresses', 'ClientController.updateAddressByClientId')
+
+  // Phone
+  Route.get('phones', 'ClientController.indexPhones')
+  Route.get('phones/:id', 'ClientController.showPhone')
+  Route.post('phones', 'ClientController.storePhone')
+  Route.put('phones/:id', 'ClientController.updatePhone')
+  Route.delete('phones/:id', 'ClientController.deletePhone')
+  Route.get('clients/:client_id/phones', 'ClientController.showPhonesByClientId')
+  Route.put('clients/:client_id/phones', 'ClientController.updatePhoneByClientId')
+
+  // Product
+  Route.get('products', 'ProductController.index')
+  Route.get('products/:id', 'ProductController.show')
+  Route.post('products', 'ProductController.store')
+  Route.put('products/:id', 'ProductController.update')
+  Route.delete('products/:id', 'ProductController.delete')
+
+  // Sale and SalesProduct
+  Route.get('sales', 'SaleController.index')
+  Route.get('sales/:id', 'SaleController.show')
+  Route.post('sales', 'SaleController.store')
+  Route.put('sales/:id', 'SaleController.update')
+  Route.delete('sales/:id', 'SaleController.delete')
+
+  Route.post('sales-products', 'SaleController.storeProduct')
+  Route.put('sales-products/:id', 'SaleController.updateProduct')
+  Route.delete('sales-products/:id', 'SaleController.deleteProduct')
+}).middleware(['auth'])
+
+
